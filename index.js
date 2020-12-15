4f24124753e2a828809ace8694c529a678255c60
AbdelRaouf Dannaoui <123raoufdannaoui@gmail.com>
2020-12-15 11:08:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 class Formatter {
   //add static methods here
+  static capitalize(string ){
+  return  string .charAt(0).toUpperCase()+string .slice(1)
+  }
+
+  static sanitize(a){
+  return a.replace( /[^A-Za-z0-9 '-]/g, '')
+  }
+
+  
 }
