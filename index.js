8fa5e00be0d8b6c3ee171bc3bc913dec78d0db91
AbdelRaouf Dannaoui <123raoufdannaoui@gmail.com>
2020-12-15 11:09:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 class Formatter {
   //add static methods here
-  static  capitalize(string ){
-  return  string .charAt(0).toUpperCase()+string .slice(1)
+  static  capitalize(a ){
+  return  a .charAt(0).toUpperCase()+a .slice(1)
   }
 
   static sanitize(a){
